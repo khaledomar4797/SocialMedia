8eca13111e469598110a46cd9c47bfb093f8cd2f
Max Sigmon <max.j.sigmon@gmail.com>
2020-11-12 17:20:21 -0500

    Post methods

---
+++ SocialMedia.Models/PostCreate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Models
+{
+    public class PostCreate
+    {
+        [Required]
+        [MaxLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        [MaxLength(280)] 
+        public string Text { get; set; }
+    }
+}
